b7384782528e1c748cb27393b237ccedb1ec01da
Emilly-Dantas <125685099+Emilly-Dantas@users.noreply.github.com>
2023-04-14 13:54:40 -0300

    java

---
+++ Aula1/Exercicio01.java
@@ -0,0 +1,26 @@
+public class Exercicio01{
+
+    public static void main(String[]args) {
+    
+
+        String nome = "Emilly Luizy Teixeira Dantas";
+        String crush = "Margot Robbie";
+        String endereco = "Rua Jorge Cândito Marim";
+        int idade= 16;
+        char letra='S';
+        double altura= 1.59;
+        String telefone= "(11) 940518943";
+
+        System.out.println("Meu nome é: "+nome);
+        System.out.println("Minha crush é: "+crush);
+        System.out.println("Meu endereço é: "+endereco);
+        System.out.println("Minha idade é: "+idade+"anos");
+        System.out.println("Meu estado civil é: "+letra);
+        System.out.println("Eu tenho "+altura+"metros");
+        System.out.println("Meu telefone é: "+telefone);
+    }
+
+  }
+
+
+    
